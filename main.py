8e941a16871cf8dddd91ac0f84d3bbb840a65f57
DracoZBA <yoaldair27@gmail.com>
2024-11-22 09:24:42 -0500

    Resolver conflicto

---
+++ main.py
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi Proyecto</title>
+</head>
+<body>
+    <h1>Bienvenido a la nueva versión de nuestra aplicación web</h1>
+    <p>Este es un párrafo inicial.</p>
+</body>
+</html>
